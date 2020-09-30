a75db96569281da45b7405a3be436711cc544274
Charlie Ramirez <shinobi@Charlies-MacBook-Pro.local>
2020-09-30 18:26:31 -0500

    Agregar segues para cambiar entre Views y enviando valores a través de las variables asignadas a cada ViewController

--- BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -10,15 +10,13 @@ import UIKit
 
 class CalculateViewController: UIViewController {
 
+    var bmiValue = "0.0"
     
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     
     @IBOutlet weak var heightSlider: UISlider!
     @IBOutlet weak var weightSlider: UISlider!
-    
-    //    var actualHeight: String = ""
-//    var actualWeight: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,13 +37,17 @@ class CalculateViewController: UIViewController {
         let height = heightSlider.value
         let weight = weightSlider.value
         let bmi = weight / pow(height, 2)
-        print(bmi)
+        bmiValue = String(format: "%.1f", bmi)
         
-        
+        self.performSegue(withIdentifier: "goToResult", sender: self)
         
     }
     
-    
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = bmiValue
+        }
+    }
 }
 
