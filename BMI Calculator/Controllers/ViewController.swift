72099140defd5541c9f170afc5434542cc97a334
Charlie Ramirez <shinobi@Charlies-MacBook-Pro.local>
2020-09-30 14:31:29 -0500

    agregar funcionalidad a los slides de height y weight, cambiando el valor de sus labels respectivamente

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+//    var actualHeight: String = ""
+//    var actualWeight: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //updateUI()
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight)Kg"
+    }
+    
+    
+    func updateUI() {
+//        actualHeight = String(format: "%.2f", heightSlider.value)
+//        heightLabel.text = actualHeight + "m"
+//        actualWeight = String(format: "%.0f", weightSlider.value)
+//        WeightLabel.text = actualWeight + "Kg"
+    }
 }
 
